822ba286e80d0f23cb632c203c216fb289c78f49
Roisul <roisnewversion@gmail.com>
2025-01-21 09:43:09 +0700

    compute the previous_hash field

--- blockchain/src/blockchain/mod.rs
+++ blockchain/src/blockchain/mod.rs
@@ -1,5 +1,7 @@
 use std::time::SystemTime;
 
+use sha2::{Digest, Sha256};
+
 #[derive(Debug)]
 pub struct Block {
     pub nonce: i32,
@@ -40,6 +42,19 @@ impl Block {
         println!("previous_hash: {:?}", self.previous_hash);
         println!("transactions: {:?}", self.transactions);
     }
+
+    pub fn hash(&self) -> Vec<u8> {
+        let mut bin = Vec::<u8>::new();
+        bin.extend(self.nonce.to_be_bytes());
+        bin.extend(self.previous_hash.clone());
+        bin.extend(self.time_stamp.to_be_bytes());
+        for tx in self.transactions.iter() {
+            bin.extend(tx.clone());
+        }
+        let mut hasher = Sha256::new();
+        hasher.update(bin);
+        hasher.finalize().to_vec()
+    }
 }
 
 #[derive(Debug)]
@@ -49,17 +64,17 @@ pub struct BlockChain {
 }
 
 impl BlockChain {
-    pub fn new()-> Self {
-        let mut bc = BlockChain{
+    pub fn new() -> Self {
+        let mut bc = BlockChain {
             transaction_pool: Vec::<Vec<u8>>::new(),
             chain: Vec::<Block>::new(),
         };
 
-        bc.create_block(0, "Hash for very first block".to_string().into_bytes());
+        bc.create_block(0, vec![0 as u8; 32]);
         bc // no semicolon
     }
 
-    pub fn create_block(&mut self, nonce:i32, previous_hash:Vec<u8>){
+    pub fn create_block(&mut self, nonce: i32, previous_hash: Vec<u8>) {
         let b = Block::new(nonce, previous_hash);
         self.chain.push(b)
     }
@@ -70,5 +85,12 @@ impl BlockChain {
             block.print();
         }
         println!("{}", "*".repeat(25));
+    }
+
+    pub fn last_block(&self) -> &Block {
+        if self.chain.len() > 1 {
+            return &self.chain[self.chain.len() - 1];
+        }
+        &self.chain[0]
     }
 }
--- blockchain/src/main.rs
+++ blockchain/src/main.rs
@@ -1,20 +1,27 @@
 pub mod blockchain;
 use blockchain::BlockChain;
-
+use sha2::{Digest, Sha256};
 fn main() {
+    let mut hasher = Sha256::new();
+    hasher.update(b"hello world\n");
+    let result = hasher.finalize();
+    println!("The result is : {:x}", result);
     // convert a string into bytes array
     // convert it to String, into_bytes() => Vec<u8>
     // let b = Block::new(0, "this is out first block!".to_string().into_bytes());
     // b.print();
-    //
     // println!("the first block is : {:?}", b);
 
     let mut block_chain = BlockChain::new();
     println!("Block chain: {:?}", block_chain);
 
     block_chain.print();
-    block_chain.create_block(1, "hash 1".to_string().into_bytes());
+
+    let previous_hash = block_chain.last_block().hash();
+    block_chain.create_block(1, previous_hash);
     block_chain.print();
-    block_chain.create_block(2, "hash 2".to_string().into_bytes());
+
+    let previous_hash = block_chain.last_block().hash();
+    block_chain.create_block(2, previous_hash);
     block_chain.print();
 }
